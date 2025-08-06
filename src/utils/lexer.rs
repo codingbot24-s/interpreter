64b314480a223849c3981e2f202c1a0a8bfc0bc0
saadfromvoid <primecoder8@gmail.com>
2025-08-07 00:25:19 +0530

    lexer with read char

--- src/utils/lexer.rs
+++ src/utils/lexer.rs
@@ -1,20 +1,36 @@
 
 struct Lexer {
     input :String,
-    position :i32, // index of current character
-    read_position:i32, // index of next character
-    c:i32 // actual char
+    current_position :usize, // index of current character
+    next_position :usize, // index of next character
+    current_char :u8, // current character
 }
 
 impl Lexer {
     fn new (&mut self,input:String) -> Self {
-        let l = Lexer {
-            input,
-            position:0,
-            read_position:0,
-            c:0,
-        };
-
+       let mut l = Lexer { 
+            input: input, 
+            current_position: 0, 
+            next_position: 0, 
+            current_char: 0 
+        }; 
+        l.read_char();
         l
     }
+
+    fn read_char (&mut self) {
+        // check end of input
+        if self.next_position >= self.input.len() {
+            self.current_char = 0
+        }
+        else {
+            self.current_char = self.input.as_bytes()[self.next_position];   
+        }
+
+        self.current_position = self.next_position;
+        self.next_position +=1;
+    }
+    
 }
+
+
