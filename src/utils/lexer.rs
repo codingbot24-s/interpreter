70d165d7d4c60d323239bef0db9cbbf267ebe52d
saadfromvoid <primecoder8@gmail.com>
2025-08-08 22:31:54 +0530

    new token done

--- src/utils/lexer.rs
+++ src/utils/lexer.rs
@@ -1,36 +1,65 @@
+use super::token::token::{Token, TokenType, new_token};
 
 struct Lexer {
-    input :String,
-    current_position :usize, // index of current character
-    next_position :usize, // index of next character
-    current_char :u8, // current character
+    input: String,
+    position: usize,
+    read_position: usize,
+    ch: char,
 }
 
+enum LE {
+    UNDEFINEDTOKEN,
+}
+
+static TOKEN_TABLE: [TokenType; 256] = {
+    let mut table = [const { TokenType::UNDEFINED }; 256];
+    table[b'=' as usize] = TokenType::ASSIGN;
+    table[b';' as usize] = TokenType::SEMICOLON;
+    table[b'(' as usize] = TokenType::LPAREN;
+    table[b')' as usize] = TokenType::RPAREN;
+    table[b',' as usize] = TokenType::COMMA;
+    table[b'+' as usize] = TokenType::PLUS;
+    table[b'{' as usize] = TokenType::LBRACE;
+    table[b'}' as usize] = TokenType::RBRACE;
+    table[b'0' as usize] = TokenType::EOF;
+    table
+};
+
 impl Lexer {
-    fn new (&mut self,input:String) -> Self {
-       let mut l = Lexer { 
-            input: input, 
-            current_position: 0, 
-            next_position: 0, 
-            current_char: 0 
-        }; 
+    fn new(&mut self, input: String) -> Self {
+        let mut l = Lexer {
+            input: input,
+            position: 0,
+            read_position: 0,
+            ch: '0',
+        };
         l.read_char();
         l
     }
 
-    fn read_char (&mut self) {
-        // check end of input
-        if self.next_position >= self.input.len() {
-            self.current_char = 0
-        }
-        else {
-            self.current_char = self.input.as_bytes()[self.next_position];   
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = '0';
+        } else {
+            //TODO: parse the next input that is on the next read_position into the char
+            self.ch = self.input.as_bytes()[self.read_position] as char
         }
 
-        self.current_position = self.next_position;
-        self.next_position +=1;
+        self.position = self.read_position;
+        self.read_position += 1;
     }
-    
+
+    fn next_token(&mut self) -> Result<Token, LE> {
+        let ch_byte = self.ch as u8;
+        let token_type = TOKEN_TABLE[ch_byte as usize];
+
+        if token_type == TokenType::UNDEFINED {
+            self.read_char();
+            Err(LE::UNDEFINEDTOKEN)
+        } else {
+            let tok = new_token(token_type, self.ch);
+            self.read_char();
+            Ok(tok)
+        }
+    }
 }
-
-
--- src/utils/token.rs
+++ src/utils/token.rs
@@ -1,32 +1,38 @@
+pub mod token {
+    #[allow(dead_code)]
+    #[derive(PartialEq,Clone,Copy)]
+    pub enum TokenType {
+        // ILLEGAL signifies a token/character we don’t know about
+        ILLEGAL,
+        // EOF stands for “end of file”, which tells our parser later on that it can stop.
+        EOF,
+        IDENT,
+        INT,
+        ASSIGN,
+        PLUS,
+        COMMA,
+        SEMICOLON,
+        LPAREN,
+        RPAREN,
+        LBRACE,
+        RBRACE,
+        FUNCTION,
+        LET,
+        UNDEFINED
+    }
 
-pub enum Token {
-    // ILLEGAL signifies a token/character we don’t know about
-    ILLEGAL(String),
-    // EOF stands for “end of file”, which tells our parser later on that it can stop. 
-    EOF(String),
-    IDENT(String) ,
-    INT(String),
-    ASSIGN (String),
-    PLUS (String),
-    COMMA(String),
-    SEMICOLON(String),
-    LPAREN(String),
-    RPAREN(String),
-    LBRACE(String),
-    RBRACE(String),
-    FUNCTION(String),
-    LET(String),
+    pub struct Token {
+        token:TokenType,
+        litreal:String
+    }
+
+    pub fn new_token (token_type:TokenType,ch:char) -> Token{
+        Token {
+            token:token_type,
+            litreal:ch.to_string(),
+        }                
+    }
+        
 }
 
 
-
-
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    const INPUT:&str = "`=+(){},;";
-
-    
-}
