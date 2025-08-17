7808a1e207f263c87d2ae1863b1c3cdecb333110
pcodefromarch <primecoder8@gmail.com>
2025-08-17 20:54:43 +0530

    fix input in the loop

--- src/utils/repl.rs
+++ src/utils/repl.rs
@@ -2,21 +2,30 @@
 pub mod repl {
     use std::io;
 
-    use crate::utils::lexer::Lexer;
+    use crate::utils::{lexer::Lexer, token::token::{Token, TokenType}};
     pub fn start () {
     println!("Hello This is the Chimpanzi programming language!");
     println!("Feel Free to Type in Commands!");
     println!(">>");
-    let mut input:String = String::new();
+
     loop {
+        let mut input:String = String::new();
         io::stdin().read_line(&mut input);
         input = input.trim().to_string();
         println!("Input is {}",input);
         if input == "exit" {
             break;
         }
-        let l = Lexer::new(&input); 
-         
+        let mut l = Lexer::new(&input); 
+        if let Some(mut tok) = l.next_token() {
+            while tok.token != TokenType::EOF {
+               match l.next_token() {
+                Some(next_tok) =>   tok = next_tok,
+                None => println!("Error in next token") 
+               }
+               println!("{:?}",tok)
+            }
+        }
     } 
     
    }
