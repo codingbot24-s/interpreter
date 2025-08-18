6ba199d96e322880366cda1549512881d806423f
pcodefromarch <primecoder8@gmail.com>
2025-08-18 21:33:03 +0530

    prompt fix

--- src/utils/repl.rs
+++ src/utils/repl.rs
@@ -1,32 +1,38 @@
-
 pub mod repl {
-    use std::io;
+    use std::io::{self, Write};
 
-    use crate::utils::{lexer::Lexer, token::token::{Token, TokenType}};
-    pub fn start () {
-    println!("Hello This is the Chimpanzi programming language!");
-    println!("Feel Free to Type in Commands!");
-    println!(">>");
+    use crate::utils::{
+        lexer::Lexer,
+        token::token::{Token, TokenType},
+    };
+    const PROMPT: &str = ">> ";
 
-    loop {
-        let mut input:String = String::new();
-        io::stdin().read_line(&mut input);
-        input = input.trim().to_string();
-        println!("Input is {}",input);
-        if input == "exit" {
-            break;
-        }
-        let mut l = Lexer::new(&input); 
-        if let Some(mut tok) = l.next_token() {
-            while tok.token != TokenType::EOF {
-               match l.next_token() {
-                Some(next_tok) =>   tok = next_tok,
-                None => println!("Error in next token") 
-               }
-               println!("{:?}",tok)
+    pub fn start() {
+        println!("Welcome to the Chimpanzee Programming Language REPL!");
+        println!("Commands:");
+        println!("  exit, quit - Exit the REPL");
+        println!();
+        loop {
+            print!("{}", PROMPT);
+            io::stdout().flush().unwrap();
+
+            let mut input: String = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            input = input.trim().to_string();
+            println!("Input is {}", input);
+            if input == "exit" {
+                break;
+            }
+            let mut l = Lexer::new(&input);
+            if let Some(mut tok) = l.next_token() {
+                while tok.token != TokenType::EOF {
+                    match l.next_token() {
+                        Some(next_tok) => tok = next_tok,
+                        None => println!("Error in next token"),
+                    }
+                    println!("{:?}", tok)
+                }
             }
         }
-    } 
-    
-   }
+    }
 }
