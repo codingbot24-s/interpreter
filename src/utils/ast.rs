9728c4a338e5af90e20b813ef1dcee62818f96a1
pcodefromarch <primecoder8@gmail.com>
2025-08-19 22:30:15 +0530

    add some node etc

--- src/utils/ast.rs
+++ src/utils/ast.rs
@@ -1,5 +1,16 @@
 
 
 pub mod ast {
-    
+    pub trait Node {
+        fn token_literal() -> str;
+    }    
+    pub trait Statement {
+        fn statement_node();   
+    }
+    pub trait Expression {
+        fn expression_node();
+    }
+    pub struct Program {
+        
+    }
 }
