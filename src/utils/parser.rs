006967392dc8c478d2a2920161efb5512f79e489
pcodefromarch <primecoder8@gmail.com>
2025-08-24 23:19:21 +0530

    add parser function

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -1,5 +1,24 @@
 
 
 pub mod parser {
-    
+    use crate::utils::{lexer::Lexer,token::{self, token::Token}};
+
+    struct Parser {
+        l: Box<Lexer>,
+        cur_token: Token,
+        peek_token: Token
+    } 
+    impl Parser {
+        pub fn new (lexer:Lexer,) {
+            
+        }
+
+        pub fn next_token (mut self) {
+            self.cur_token = self.peek_token;
+           if let Some(tok) = self.l.next_token() {
+                self.peek_token = tok
+           } 
+        }
+    }
+
 }
